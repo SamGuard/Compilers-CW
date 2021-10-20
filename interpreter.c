38dfeb27edbf8489ad04d1d5e741d8a8f5ac540a
Samuel Guard <sg2268@bath.ac.uk>
2021-10-20 15:23:37 +0100

    ADDED TWO VARIABLES TOGETHER

--- interpreter.c
+++ interpreter.c
@@ -1,69 +1,149 @@
 #include "interpreter.h"
-#include <stdlib.h>
-#include <stdio.h>
 
-typedef struct Binding
-{
-  int value;
-  TOKEN *name;
-  struct Binding *next;
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Binding {
+    int value;
+    TOKEN *name;
+    struct Binding *next;
 } Binding;
 
-typedef struct Frame
-{
-  Binding *b;
-  struct Frame *next;
+typedef struct Frame {
+    Binding *b;
+    struct Frame *next;
 } Frame;
 
-TOKEN *evalExpression(NODE* exp, Frame *f){
-  TOKEN *t = (TOKEN*)malloc(sizeof(TOKEN));
-  
+TOKEN *allocToken(int value, int type, char *lexeme, TOKEN *next) {
+    TOKEN *x = (TOKEN *)malloc(sizeof(TOKEN));
+    if (x == NULL) {
+        perror("Cannot allocate memory");
+    }
+    x->type = type;
+    x->lexeme = lexeme;
+    x->next = next;
+    x->value = value;
+    return x;
 }
 
-void declare(TOKEN *x, Frame *f){
+void declare(TOKEN *ident, int value, Frame *f) {
+    Binding *b = f->b;
+    Binding *prev;
+    while (b != NULL) {
+        if (b->name == ident) {
+            perror("variable has already been declared");
+            exit(-1);
+        }
+        prev = b;
+        b = b->next;
+    }
 
+    Binding *newBinding = (Binding *)malloc(sizeof(Binding));
+    if (newBinding == NULL) {
+        perror("Cannot allocate memory for new binding");
+        exit(-1);
+    }
+    newBinding->value = value;
+    newBinding->name = ident;
+    newBinding->next = NULL;
+    prev->next = newBinding;
 }
 
-int traverse(NODE *tree, Frame *f)
-{
-
-  switch (tree->type)
-  {
-  default:
-    error("unexpected type");
-    return 0;
-  case 'D':
-    return traverse(tree->right, f);
-  case ';':
-    traverse(tree->left, f);
-    if(tree->right != NULL) traverse(tree->right, f);
-    return;
-  case '~':
-    traverse(tree->right, f);
-    return;
-  case '=':
-    declare(evalExpression(tree, f), f);
-    return 0;
-    
-  }
+TOKEN *retrieve(TOKEN *ident, Frame *f) {
+    Binding *b = f->b;
+    while (TRUE) {
+        if (b->name == ident) {
+            return allocToken(b->value, CONSTANT, NULL, NULL);
+        }
+        if (b->next == NULL) {
+            perror("Variable regerenced before assignment");
+        }
+        b = b->next;
+    }
 }
 
-void interpreter(NODE *tree)
-{
-  Frame *f = (Frame *)malloc(sizeof(Frame));
-  f->b = (Binding *)malloc(sizeof(Binding));
-  f->next = NULL;
+void assign(TOKEN *x, Frame *f) {}
+
+TOKEN *arithmetic(TOKEN *x, TOKEN *y, char symbol) {
+    if (x->type != CONSTANT || y->type != CONSTANT) {
+        perror("Cannot perform arithmatic on non-constants");
+    }
+
+    TOKEN *z = allocToken(0, CONSTANT, NULL, NULL);
+
+    switch (symbol) {
+        default:
+            perror("Invalid symbol");
+        case '+':
+            z->value = x->value + y->value;
+            return z;
+    }
 }
 
-void simplePrintTree(NODE *tree)
-{
-  NODE *head = tree;
-  printf("type: %c  ", head->type);
-  if (head->right == NULL)
-  {
-    printf("Leaf value: %d\n", ((TOKEN *)head->left)->value);
-  }
-  printf("\n");
-  interpreter(head->left);
-  interpreter(head->right);
+TOKEN *traverse(NODE *tree, Frame *f) {
+    printf("%c\n", tree->type);
+    switch (tree->type) {
+        default:
+            perror("unexpected type");
+        case 'D':
+            return traverse(tree->right, f);
+        case ';':
+            traverse(tree->left, f);
+            if (tree->right != NULL) traverse(tree->right, f);
+            return NULL;
+        case '~':
+            traverse(tree->right, f);
+            return NULL;
+        case '=':
+            declare((TOKEN *)tree->left->left, traverse(tree->right, f)->value,
+                    f);
+            return NULL;
+        case '+':
+            return arithmetic(traverse(tree->left, f), traverse(tree->right, f),
+                              '+');
+        case LEAF:;
+            TOKEN *t = (TOKEN *)tree->left;
+            if (t->type == CONSTANT) {
+                return t;
+            } else if (t->type == IDENTIFIER) {
+                return retrieve(t, f);
+            }
+    }
+}
+
+void simplePrintTree(NODE *tree) {
+    NODE *head = tree;
+    if (head->type == LEAF) {
+        TOKEN *t = ((TOKEN *)head->left);
+        if (t->type == IDENTIFIER) {
+            printf("Leaf value: %s\n", t->lexeme);
+        } else {
+            printf("Leaf value: %d\n", t->value);
+        }
+        return;
+    }
+    printf("type: %c  \n", head->type);
+    interpreter(head->left);
+    interpreter(head->right);
+}
+
+void printFrame(Frame *f) {
+    while (f != NULL) {
+        Binding *b = f->b;
+        while (b != NULL) {
+            if (b->name != NULL)
+                printf("%d %s = %d, ", b->name->type, b->name->lexeme, b->value);
+            b = b->next;
+        }
+        f = f->next;
+    }
+}
+
+void interpreter(NODE *tree) {
+    Frame *f = (Frame *)malloc(sizeof(Frame));
+    f->b = (Binding *)malloc(sizeof(Binding));
+    f->next = NULL;
+    traverse(tree, f);
+    // simplePrintTree(tree);
+    printFrame(f);
 }
--- interpreter.h
+++ interpreter.h
@@ -1,5 +1,9 @@
+#ifndef __INTERPRETER_H
+#define __INTERPRETER_H
 #include "./lexer_parser/nodes.h"
 #include "./lexer_parser/C.tab.h"
 #include "./lexer_parser/token.h"
 
-int interpreter(NODE *tree);
+void interpreter(NODE *tree);
+
+#endif
